2189a174c22cf53fa1018a8340ffad50b867c3bb
Dennis Enraca <37585149+DEnraca@users.noreply.github.com>
2025-02-17 13:52:12 +0800

    Handle-user_Secure-Password_Handling-User_Keyboard-Type_Limit-Input_Auto-Focus_Adding-Icon_Input-Error

---
+++ text-input-properties/App.js
@@ -0,0 +1,121 @@
+import React, {useState} from 'react';
+import {View, TextInput, StyleSheet, Text, Button, Alert} from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+
+const HanleInputExample = () => {
+
+  const [fname, setFname] = useState('');
+  const [lname, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+ const handleSubmit = () =>{
+    Alert.alert('Registration Details: ', 'First Name: ' + fname  + '\nLast Name: ' + lname + '\nAge: ' + age + '\nEmail: ' +email)
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}> Registration Form</Text>
+      <View style={styles.inputContainer}>
+            <Ionicons name="person-circle-outline" size={20} color="gray" style={styles.icon}/>
+        <TextInput
+          style = {styles.input}
+          placeholder = "First Name"
+          onChangeText = {(value) => setFname(value)}
+          autoFocus={true}
+          maxLength={30}
+        /> 
+      </View>
+       <View style={styles.inputContainer}>
+            <Ionicons name="person-circle-outline" size={20} color="gray" style={styles.icon}/>
+        <TextInput
+          style = {styles.input}
+          placeholder = "Last Name"
+          onChangeText = {(value) => setName(value)}
+          maxLength={30}
+        /> 
+      </View>
+       <View style={styles.inputContainer}>
+            <Ionicons name="calendar-number-outline" size={20} color="gray" style={styles.icon}/>
+        <TextInput
+          style = {styles.input}
+          placeholder = "Age"
+          keyboardType="phone-pad"
+          onChangeText = {(value) => setAge(value)}
+        /> 
+      </View>
+       <View style={styles.inputContainer}>
+            <Ionicons name="mail-outline" size={20} color="gray" style={styles.icon}/>
+        <TextInput
+          style = {styles.input}
+          placeholder = "Email"
+          keyboardType="email-address"
+          maxLength={30}
+          onChangeText = {(value) => setEmail(value)}
+        /> 
+      </View>
+       <View style={styles.inputContainer}>
+            <Ionicons name="key-outline" size={20} color="gray" style={styles.icon}/>
+        <TextInput
+          style = {styles.input}
+          secureTextEntry = {true}
+          placeholder = "Password"
+          maxLength={30}
+          onChangeText = {(value) => setPassword(value)}
+        /> 
+        
+      </View>
+        <Button
+          title="Register"
+          onPress={handleSubmit}
+        />
+
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    height: 40,
+    flex: 1,
+    fontSize: 16,
+    paddingHorizontal: 3,
+    paddingVertical:3,
+
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical:10,
+    margin: 3
+  },
+  icon : {
+    marginRight: 10
+  },
+  header : {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize:16
+  }
+  // displayText: {
+  //   marginTop: 10,
+  //   fontSize: 10
+  // }
+});
+
+export default HanleInputExample;
